e93518efe909eab70c8da5c2e67ed54509b3f8cb
Sonu Ma <sonumalmb@gmail.com>
2025-06-17 20:35:30 +0545

    Admin panel

---
+++ resources/views/components/frontend-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('frontend/style.css')}}">
+</head>
+<body>
+    <header>
+        <x-frontend-navbar/>
+    </header>
+    <main>
+        {{ $slot }}
+    </main>
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+</html>
